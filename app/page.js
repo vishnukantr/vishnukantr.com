2e7bd9414e03ef126c791ef8fff8f53830fa234f
vishnukantr <vishnu.ramachandran95@gmail.com>
2026-03-01 00:45:10 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { motion, useScroll, useTransform } from "framer-motion";
 import { Reveal } from "@/components/Reveal";
 import { useEffect, useState } from "react";
 
-const HERO_IMAGE = "/images/hero.jpg";   
+const HERO_IMAGE = "/images/hero.jpg";  
 const AMWAAJ_IMAGE = "/images/amwaajessakina.jpg";
 
 function Nav() {
